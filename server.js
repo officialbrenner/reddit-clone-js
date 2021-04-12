098652df0c3405314752f3f8cb5d520f4eaac4e4
Brenner Haverlock <80720533+theoneking01@users.noreply.github.com>
2021-04-12 16:08:08 -0700

    init

---
+++ server.js
@@ -0,0 +1,35 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+
+// Routes
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3007!');
+});
+
+app.use(express.static('public'));
+  // app.js
+  // Require Libraries
+
+  app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    // Tenor.Search.Query(term, "10")
+    //     .then(response => {
+    //         // store the gifs we get back from the search
+    //         const gifs = response;
+    //         // pass the gifs as an object into the home page
+    //         res.render('home', { gifs })
+    //     }).catch(console.error)
+  })
